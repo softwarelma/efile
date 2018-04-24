faa8a5b9c141f9161bbbe4e0ea73820bfb1fdab9
Maria Teresa Manca <Maria Teresa Manca@10.0.74.36>
2018-04-24 11:04:28 +0200

    Add bold colored title, background color and font color

--- src/main/java/com/softwarelma/efile/EfileServer.java
+++ src/main/java/com/softwarelma/efile/EfileServer.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import com.softwarelma.epe.p1.app.EpeAppException;
 import com.softwarelma.epe.p1.app.EpeAppUtils;
 import com.softwarelma.epe.p3.disk.EpeDiskFinalList_files_recursive;
+import com.sun.prism.Image;
 import com.vaadin.data.TreeData;
 import com.vaadin.data.provider.InMemoryDataProvider;
 import com.vaadin.data.provider.TreeDataProvider;
@@ -22,118 +23,123 @@ import com.vaadin.ui.Tree.ItemClick;
 import com.vaadin.ui.Tree.ItemClickListener;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Label;
 
 public class EfileServer implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    private static EfileServer server = new EfileServer();
-    private Map<String, EfileTreeData> mapPathAndTreeData;
-    private Button buttonDownload;
-    private Extension extension;
-    private String folderPath = "/home/ellison/Downloads/TCM/epe/";// TODO
-    private String defaultFileName;
+	private static final long serialVersionUID = 1L;
+	private static EfileServer server = new EfileServer();
+	private Map<String, EfileTreeData> mapPathAndTreeData;
+	private Button buttonDownload;
+	private Extension extension;
+	//private String folderPath = "C:/Users/Maria Teresa Manca/Desktop/medcis/MEDCIS_NEW_1";// TODO
+	private String folderPath = "C:/Users/Administrator/Desktop/MEDCIS";
+	private String defaultFileName;
 
-    public static EfileServer getInstance() throws EpeAppException {
-        return EfileServer.server;
-    }
+	public static EfileServer getInstance() throws EpeAppException {
+		return EfileServer.server;
+	}
 
-    private EfileServer() {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("log4j.properties").getFile());
-        this.defaultFileName = file.getAbsolutePath();
-    }
+	private EfileServer() {
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource("log4j.properties").getFile());
+		this.defaultFileName = file.getAbsolutePath();
+	}
 
-    // https://vaadin.com/docs/v8/framework/datamodel/datamodel-hierarchical.html
-    public void loadPage(UI ui) throws EpeAppException {
-        // ui.setContent(new Label("hw"));
-        Tree<EfileTreeData> tree = this.newTree();
-        VerticalLayout layout = new VerticalLayout();
-        this.newDownloadButton();
-        layout.addComponents(tree, this.buttonDownload);
-        ui.setContent(layout);
-    }
+	// https://vaadin.com/docs/v8/framework/datamodel/datamodel-hierarchical.html
+	public void loadPage(UI ui) throws EpeAppException {
+		// ui.setContent(new Label("hw"));
+		Label title = new Label("MEDCIS PROJECT LAYERS");
+		Tree<EfileTreeData> tree = this.newTree();
+		VerticalLayout layout = new VerticalLayout();
+		this.newDownloadButton();
+		layout.addComponent(title);
+		layout.addComponents(tree, this.buttonDownload);
+		ui.setContent(layout);
 
-    private Tree<EfileTreeData> newTree() throws EpeAppException {
-        // 1
-        Tree<EfileTreeData> tree = new Tree<>();
-        this.addItemListener(tree);
+	}
 
-        // 2
-        TreeData<EfileTreeData> treeData = new TreeData<>();
-        this.addItems(treeData);
+	private Tree<EfileTreeData> newTree() throws EpeAppException {
+		// 1
+		Tree<EfileTreeData> tree = new Tree<>();
+		this.addItemListener(tree);
 
-        // 3
-        this.addDataProvider(tree, treeData);
+		// 2
+		TreeData<EfileTreeData> treeData = new TreeData<>();
+		this.addItems(treeData);
 
-        return tree;
-    }
+		// 3
+		this.addDataProvider(tree, treeData);
 
-    private void addItemListener(Tree<EfileTreeData> tree) {
-        tree.addItemClickListener(new ItemClickListener<EfileTreeData>() {
+		return tree;
+	}
 
-            private static final long serialVersionUID = 1L;
+	private void addItemListener(Tree<EfileTreeData> tree) {
+		tree.addItemClickListener(new ItemClickListener<EfileTreeData>() {
 
-            @Override
-            public void itemClick(ItemClick<EfileTreeData> event) {
-                setFileDownloader(event.getItem());
-            }
+			private static final long serialVersionUID = 1L;
 
-        });
-    }
+			@Override
+			public void itemClick(ItemClick<EfileTreeData> event) {
+				setFileDownloader(event.getItem());
+			}
 
-    private void addItems(TreeData<EfileTreeData> treeData) throws EpeAppException {
-        this.mapPathAndTreeData = new HashMap<>();
-        List<String> listFileOrDir = EpeDiskFinalList_files_recursive.listFilesRecursive(this.folderPath);
-        EfileTreeData itemParent, itemChild;
-        Map.Entry<String, String> filePathAndName;
-        String parent, child;
-        File fileChild;
+		});
+	}
 
-        for (String fileOrDir : listFileOrDir) {
-            fileChild = new File(fileOrDir);
-            if (!fileChild.isDirectory() && !fileChild.isFile())
-                continue;
-            filePathAndName = EpeAppUtils.retrieveFilePathAndName(fileOrDir);
-            parent = EpeAppUtils.cleanDirName(filePathAndName.getKey());
-            child = filePathAndName.getValue();
-            itemChild = new EfileTreeData(fileChild.isDirectory(), parent, child);
-            itemParent = this.mapPathAndTreeData.get(parent);// could be null
-            treeData.addItem(itemParent, itemChild);
+	private void addItems(TreeData<EfileTreeData> treeData) throws EpeAppException {
+		this.mapPathAndTreeData = new HashMap<>();
+		List<String> listFileOrDir = EpeDiskFinalList_files_recursive.listFilesRecursive(this.folderPath);
+		EfileTreeData itemParent, itemChild;
+		Map.Entry<String, String> filePathAndName;
+		String parent, child;
+		File fileChild;
 
-            if (fileChild.isDirectory()) {
-                fileOrDir = EpeAppUtils.cleanDirName(fileOrDir);
-                this.mapPathAndTreeData.put(fileOrDir, itemChild);
-            }
-        }
-    }
+		for (String fileOrDir : listFileOrDir) {
+			fileChild = new File(fileOrDir);
+			if (!fileChild.isDirectory() && !fileChild.isFile())
+				continue;
+			filePathAndName = EpeAppUtils.retrieveFilePathAndName(fileOrDir);
+			parent = EpeAppUtils.cleanDirName(filePathAndName.getKey());
+			child = filePathAndName.getValue();
+			itemChild = new EfileTreeData(fileChild.isDirectory(), parent, child);
+			itemParent = this.mapPathAndTreeData.get(parent);// could be null
+			treeData.addItem(itemParent, itemChild);
 
-    private void addDataProvider(Tree<EfileTreeData> tree, TreeData<EfileTreeData> treeData) {
-        InMemoryDataProvider<EfileTreeData> inMemoryDataProvider = new TreeDataProvider<>(treeData);
-        tree.setDataProvider(inMemoryDataProvider);
-        // tree.expand("...");
-    }
+			if (fileChild.isDirectory()) {
+				fileOrDir = EpeAppUtils.cleanDirName(fileOrDir);
+				this.mapPathAndTreeData.put(fileOrDir, itemChild);
+			}
+		}
+	}
 
-    private void setFileDownloader(EfileTreeData treeData) {
-        if (treeData.isDir()) {
-            this.buttonDownload.setEnabled(false);
-            return;
-        }
+	private void addDataProvider(Tree<EfileTreeData> tree, TreeData<EfileTreeData> treeData) {
+		InMemoryDataProvider<EfileTreeData> inMemoryDataProvider = new TreeDataProvider<>(treeData);
+		tree.setDataProvider(inMemoryDataProvider);
+		// tree.expand("...");
+	}
 
-        this.buttonDownload.setEnabled(true);
-        Resource res = new FileResource(new File(treeData.getFilePath() + treeData.getFileName()));
-        FileDownloader fd = new FileDownloader(res);
-        this.buttonDownload.removeExtension(this.extension);
-        fd.extend(this.buttonDownload);
-        this.extension = fd;
-    }
+	private void setFileDownloader(EfileTreeData treeData) {
+		if (treeData.isDir()) {
+			this.buttonDownload.setEnabled(false);
+			return;
+		}
 
-    private void newDownloadButton() {
-        this.buttonDownload = new Button("Download");
-        this.buttonDownload.setEnabled(false);
-        Resource res = new FileResource(new File(this.defaultFileName));
-        FileDownloader fd = new FileDownloader(res);
-        fd.extend(this.buttonDownload);
-        this.extension = fd;
-    }
+		this.buttonDownload.setEnabled(true);
+		Resource res = new FileResource(new File(treeData.getFilePath() + treeData.getFileName()));
+		FileDownloader fd = new FileDownloader(res);
+		this.buttonDownload.removeExtension(this.extension);
+		fd.extend(this.buttonDownload);
+		this.extension = fd;
+	}
+
+	private void newDownloadButton() {
+		this.buttonDownload = new Button("Download");
+		this.buttonDownload.setEnabled(false);
+		Resource res = new FileResource(new File(this.defaultFileName));
+		FileDownloader fd = new FileDownloader(res);
+		fd.extend(this.buttonDownload);
+		this.extension = fd;
+	}
 
 }
